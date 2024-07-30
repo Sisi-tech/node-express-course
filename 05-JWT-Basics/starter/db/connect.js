6b31677b5d56010102340ce545f296ab51495447
Sisi Wang <sisiwang242@gmail.com>
2024-07-29 22:45:23 -0400

    assignment for week8 - JWT

--- 05-JWT-Basics/starter/db/connect.js
+++ 05-JWT-Basics/starter/db/connect.js
@@ -2,10 +2,18 @@ const mongoose = require('mongoose')
 
 const connectDB = (url) => {
   return mongoose.connect(url, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-    useUnifiedTopology: true,
+    // useNewUrlParser: true,
+    // useCreateIndex: true,
+    // useFindAndModify: false,
+    // useUnifiedTopology: true,
+    serverSelectionTimeoutMS: 5000,
+    socketTimeoutMS: 45000,
+  })
+  .then(() => {
+    console.log("Connected to the DB...");
+  })
+  .catch((err) => {
+    console.error("Database connection Error: ", err);
   })
 }
 
